a121057bb81c93f0ac71b14c1b8356ee82dd3ba5
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2024-06-29 23:28:18 +0300

    refactor add todo function and add displayCurrent function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,14 @@ async function getApiKey() {
   response = await response.json();
   let apiKey = response.apiKey;
   localStorage.setItem("apiKey", apiKey);
-  console.log(apiKey);
 }
 
 function getUserData() {
   if (apiKey == null) {
     getApiKey();
+  } else {
+    allTodos = getTodos();
+    displayCurrent();
   }
 
   if (username == null) {
@@ -26,34 +28,40 @@ function getUserData() {
   document.querySelector("header h3").textContent = `Hey, ${username}`;
 }
 
-async function addTodo() {
-  let todo = document.querySelector("input");
-  document
-    .querySelector(".submit-todo")
-    .addEventListener("click", async function () {
-      let todoVal = todo.value;
-      todo.value = "";
-      let response = await fetch(`https://todos.routemisr.com/api/v1/todos`, {
-        method: "POST",
-        body: JSON.stringify({
-          title: todoVal,
-          apiKey: apiKey,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      response = await response.json();
-      // TODO use a better was of informing the user his todo was added
-      if (response.message == "success") {
-        alert("Your Todo Was Added");
-      } else {
-        alert("Please Try Again");
-      }
-    });
-  allTodos = await getTodos();
-  console.log(allTodos);
+async function addTodo(todoVal) {
+  let response = await fetch(`https://todos.routemisr.com/api/v1/todos`, {
+    method: "POST",
+    body: JSON.stringify({
+      title: todoVal,
+      apiKey: apiKey,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  response = await response.json();
+  return response;
 }
+
+// Adding a task
+document
+  .querySelector(".submit-todo")
+  .addEventListener("click", async function () {
+    let todo = document.querySelector("input");
+
+    response = await addTodo(todo.value);
+
+    todo.value = "";
+
+    // TODO use a better was of informing the user his todo was added
+    if (response.message == "success") {
+      alert("Your Todo Was Added");
+    } else {
+      alert("Please Try Again");
+    }
+
+    displayCurrent();
+  });
 
 async function getTodos() {
   let response = await fetch(
@@ -63,10 +71,25 @@ async function getTodos() {
   return response.todos;
 }
 
-async function main() {
+async function displayCurrent() {
+  let lstGroup = document.querySelector(".list-group");
   allTodos = await getTodos();
-  getUserData();
-  addTodo();
+  lstGroup.innerHTML = "";
+  for (const todo of allTodos) {
+    if (!todo.completed) {
+      lstGroup.innerHTML += `
+      <li
+        class="list-group-item bg-black bg-opacity-10 d-flex justify-content-between align-items-center">
+        <span>
+          <i class="fa-regular fa-square me-2"></i> ${todo.title} 
+        </span>
+        <span>
+          <i class="fa-regular fa-trash-can"></i>
+        </span>
+      </li>`;
+    }
+  }
 }
 
-main();
+getUserData();
+displayCurrent();
